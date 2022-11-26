a5d6f8e4698e92ecf98eb9220ca644414624684b
litvinovDanil112011 <danil112011@mail.ru>
2022-11-26 21:15:19 +0300

    Initial Commit

---
+++ MemoryCross/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryCross
+//
+//  Created by Daniil Litvinov on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
